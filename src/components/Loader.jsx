100e48c74a18c76e6adedbe6e2906f2db8743e9d
Tiema Coulibaly <tiemacouly@outlook.com>
2022-05-26 21:14:37 +0200

    create a loader component

---
+++ src/components/Loader.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Loader = () => {
+  let circleCommonClasses = "h-2 w-2 bg-white   rounded-full";
+  return (
+    <div className="flex p-2">
+      <div className={`${circleCommonClasses} mr-1 animate-bounce`}></div>
+      <div className={`${circleCommonClasses} mr-1 animate-bounce200`}></div>
+      <div className={`${circleCommonClasses} animate-bounce400`}></div>
+    </div>
+  );
+};
+
+export default Loader;
