a11c070e94495d887dd0c4eaa24628834e7c45d4
Tiema Coulibaly <tiemacouly@outlook.com>
2022-05-26 22:51:42 +0200

    update the style of not found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,25 +1,39 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const NotFound = () => {
-	return (
-		<div className="bg-gradient-to-r from-green-400 to-blue-500 text-white min-h-screen flex items-center">
-			<div className="container mx-auto p-4 flex flex-wrap items-center">
-				<div className="w-full md:w-7/12 text-center md:text-left p-4">
-					<div className="text-6xl font-medium">404</div>
-					<div className="text-xl md:text-3xl font-medium mb-4">
-						Oops. This page has gone missing.
-					</div>
-					<div className="text-lg mb-8">
-						You may have mistyped the address or the page may have
-						moved.
-					</div>
-					<a href="#ff" className="border border-white rounded p-4">
-						Go Home
-					</a>
-				</div>
-			</div>
-		</div>
-	);
-};
+const NotFound = () => (
+  <div
+    className="
+    flex
+    items-center
+    justify-center
+    w-full
+    h-16
+    lg:h-auto md:h-auto
+    bg-gradient-to-r
+    from-green-100 to-green-200
+    py-28
+  ">
+    <div className="px-10 lg:px-40 md:px-40 py-5 lg:py-20 md:py-20 bg-white rounded-md shadow-xl">
+      <div className="flex flex-col items-center">
+        <h1 className="font-bold text-green-600 text-9xl">404</h1>
+
+        <h6 className="mb-2 text-2xl font-bold text-center text-gray-800 md:text-3xl">
+          <span className="text-green-400">Oops!</span> Page not found
+        </h6>
+
+        <p className="mb-8 text-center text-gray-500 md:text-lg">
+          The page you’re looking for doesn’t exist.
+        </p>
+
+        <Link
+          to="/"
+          className="group relative w-full flex justify-center p-3 text-xl font-medium rounded-md text-white bg-gradient-to-r from-green-500 to-green-800  hover:bg-green-600 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">
+          Go home
+        </Link>
+      </div>
+    </div>
+  </div>
+);
 
 export default NotFound;
