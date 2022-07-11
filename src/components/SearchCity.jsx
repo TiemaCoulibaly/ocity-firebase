e05cd083f4618f18fff9ba2dccb8ed77f530fddb
Tiema Coulibaly <tiemacouly@outlook.com>
2022-07-11 19:55:54 +0200

    :sparkles: introduce search feature

---
+++ src/components/SearchCity.jsx
@@ -0,0 +1,14 @@
+const SearchCity = ({ searchTerm, setSearchTerm }) => {
+  return (
+    <div className="flex justify-center">
+      <input
+        className="w-full mt-5 lg:w-1/5 md:w-1/5 sm:w-1/5 text-gray-700 p-2 mb-4 leading-tight focus:border-green-600  rounded-lg border  border-green-500"
+        type="text"
+        placeholder="Entrez une ville..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+    </div>
+  );
+};
+export default SearchCity;
