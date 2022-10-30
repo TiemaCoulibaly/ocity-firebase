50924e7ea22aaf5cc7512c7dfd8ee4bb7508418c
Tiema Coulibaly <tiemacouly@outlook.com>
2022-10-30 13:59:28 +0100

    upgrade react 17 to react 18 :arrow_up:

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 import { AuthContextProvider } from "./context/AuthContext";
 import "./index.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "leaflet/dist/leaflet.css";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
   <AuthContextProvider>
     <App />
-  </AuthContextProvider>,
-  document.getElementById("root")
+  </AuthContextProvider>
 );
